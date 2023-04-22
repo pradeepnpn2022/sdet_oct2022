610dc44a1a494e820810b124019caf56c98b299f
pradeepnpn2022 <105690054+pradeepnpn2022@users.noreply.github.com>
2023-04-22 12:00:54 +0530

    Replaced Thread.sleep

--- upload.java
+++ upload.java
@@ -17,11 +17,11 @@ public class UploadFileTest2
 		Robot robot = new Robot();
 		StringSelection ss = new StringSelection(System.getProperty("user.dir")+ File.separator + "src" + File.separator + "test" + File.separator + "resources" + File.separator + "screenshots" + File.separator + "test.png");
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
+		Thread.sleep(3000);
 		Thread.sleep(Duration.ofSeconds(5));
 		robot.keyPress(KeyEvent.VK_CONTROL);
 		robot.keyPress(KeyEvent.VK_V);
 		robot.keyRelease(KeyEvent.VK_V);
-		Thread.sleep(3000);
 		robot.keyRelease(KeyEvent.VK_CONTROL);
 		Thread.sleep(Duration.ofSeconds(5));
 		robot.keyPress(KeyEvent.VK_ENTER);
